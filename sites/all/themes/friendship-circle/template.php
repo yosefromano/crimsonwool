0bff14e503cbfdc3a07158d5547f0c3f0c9d60e3
Yosef Romano <yosefromano@gmail.com>
2015-12-03 14:02:14 -0800

    fc template edits

--- sites/all/themes/friendship-circle/template.php
+++ sites/all/themes/friendship-circle/template.php
@@ -13,4 +13,44 @@ $('.meerkat').meerkat({
 
 });", array('type' => 'inline', 'scope' => 'footer', 'weight' => 16));
 
+function myfctheme_theme() {
+  $items = array();
+    
+  
+  $items['user_login'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'myfctheme') . '/templates',
+    'template' => 'user-login',
+    'preprocess functions' => array(
+       'myfctheme_preprocess_user_login'
+    ),
+	
+  );
+
+  $items['user_pass'] = array(
+    'render element' => 'form',
+    'path' => drupal_get_path('theme', 'myfctheme') . '/templates',
+    'template' => 'user-pass',
+    'preprocess functions' => array(
+      'myfctheme_preprocess_user_pass'
+    ),
+	 
+  );
+  return $items;
+  
+}
+
+/*function myfctheme_preprocess_user_login(&$vars) {
+  $vars['intro_text'] = t('This is my awesome login form');
+}
+
+function myfctheme_preprocess_user_register_form(&$vars) {
+  $vars['intro_text'] = t('This is my super awesome reg form');
+}
+
+function myfctheme_preprocess_user_pass(&$vars) {
+  $vars['intro_text'] = t('This is my super awesome request new password form');
+}
+*/
+
 ?>
