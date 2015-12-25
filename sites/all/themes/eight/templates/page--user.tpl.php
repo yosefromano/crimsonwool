be3ccbc8fd4cb2a6c4acd503f60f80a168ff9577
Yosef Romano <yosefromano@gmail.com>
2015-12-24 20:15:33 -0800

    theme updates

---
+++ sites/all/themes/eight/templates/page--user.tpl.php
@@ -0,0 +1,26 @@
+
+<div class="wrapper">
+	<a  href="/user"><div class="cloud-logo">
+		
+	</div></a>
+  <div class="row front">
+	
+	  
+<?php print render($page['content']);?>
+</div>
+
+
+	<ul class="bg-bubbles">
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+		<li></li>
+	</ul>
+</div>
+	
