6c7e91543382a8fadb22116aa0f78ede33f3b04c
Guy Oron <guyoron@gmail.com>
2018-01-14 15:01:55 -1000

    Patch system status message permissions

--- profiles/civicrm_starterkit/modules/civicrm/CRM/Utils/Check.php
+++ profiles/civicrm_starterkit/modules/civicrm/CRM/Utils/Check.php
@@ -80,7 +80,7 @@ class CRM_Utils_Check {
    * Display daily system status alerts (admin only).
    */
   public function showPeriodicAlerts() {
-    if (CRM_Core_Permission::check('administer CiviCRM')) {
+    if (CRM_Core_Permission::check('administer restricted CiviCRM features')) {
       $session = CRM_Core_Session::singleton();
       if ($session->timer('check_' . __CLASS__, self::CHECK_TIMER)) {
 
