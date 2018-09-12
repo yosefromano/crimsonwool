16d83fbc9c160b03e94211f703444ba07b9bb35f
Herb vd Dool <herb@freeform.ca>
2018-09-12 14:48:44 -0400

    Update to CiviCRM 5.5.3. This release includes a number of bug fixes and improvements, including database changes. Read more about the changes: https://civicrm.org/blog/dev-team/announcing-civicrm-55-release and https://civicrm.org/blog/dev-team/announcing-civicrm-54-release.
    Make sure you backup using Pantheon's database backup tool first. Then either go to http://<your_drupal_home>/civicrm/upgrade?reset=1 or use terminus drush site.env civicrm-upgrade-db. Fully test on a dev environment before upgrading on live. Don't merge this code yet if you've got other updates which are urgent.
    
    If you have questions contact http://civicrmstarterkit.org/contact. We provide some basic general support for the public. If you require help with your specific website there will likely be a cost.

--- profiles/civicrm_starterkit/modules/civicrm/vendor/autoload.php
+++ profiles/civicrm_starterkit/modules/civicrm/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit61d8e138f140ca7ed358873ad7e10d15::getLoader();
+return ComposerAutoloaderInit437ca3b0f4619c3058aaa7d4b56b01d9::getLoader();
