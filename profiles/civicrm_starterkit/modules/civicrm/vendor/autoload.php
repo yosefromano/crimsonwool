19c6e245ef4ad1ef360aeb634bf3aa2b374246d6
Herb vd Dool <herb@3speedhub.com>
2019-01-21 11:23:23 -0500

    Update to CiviCRM 5.7.3. This release includes bug fixes. No database changes. Read more about the changes: https://civicrm.org/blog/dev-team/civicrm-570-release.
        Make sure you backup using Pantheon's backup tool first. Fully test on a dev environment before upgrading on live. Don't merge this code yet if you've got other updates which are urgent.
    
        If you have questions contact https://civicrmstarterkit.org/contact. We provide some basic general support for the public. If you require help with your specific website there will likely be a cost.

--- profiles/civicrm_starterkit/modules/civicrm/vendor/autoload.php
+++ profiles/civicrm_starterkit/modules/civicrm/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit394e23a23dd6b1e8abe074c6696f65e9::getLoader();
+return ComposerAutoloaderInitbf1d65642e26d59a4e3a84ae29d7004d::getLoader();
--- profiles/civicrm_starterkit/modules/civicrm/vendor/composer/autoload_static.php
+++ profiles/civicrm_starterkit/modules/civicrm/vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit394e23a23dd6b1e8abe074c6696f65e9
+class ComposerStaticInitbf1d65642e26d59a4e3a84ae29d7004d
 {
     public static $files = array (
         '320cde22f66dd4f5d3fd621d3e88b98f' => __DIR__ . '/..' . '/symfony/polyfill-ctype/bootstrap.php',
@@ -397,10 +397,10 @@ class ComposerStaticInit394e23a23dd6b1e8abe074c6696f65e9
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit394e23a23dd6b1e8abe074c6696f65e9::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit394e23a23dd6b1e8abe074c6696f65e9::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit394e23a23dd6b1e8abe074c6696f65e9::$prefixesPsr0;
-            $loader->classMap = ComposerStaticInit394e23a23dd6b1e8abe074c6696f65e9::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitbf1d65642e26d59a4e3a84ae29d7004d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitbf1d65642e26d59a4e3a84ae29d7004d::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitbf1d65642e26d59a4e3a84ae29d7004d::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitbf1d65642e26d59a4e3a84ae29d7004d::$classMap;
 
         }, null, ClassLoader::class);
     }
